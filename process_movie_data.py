75ee188bb6dfcba87e29dd4b896186d15079a474
Tonyaradzwa <tonyaradzwa@gmail.com>
2023-10-11 21:11:26 -0700

    Changed find_top_five function name and updated comments

--- process_movie_data.py
+++ process_movie_data.py
@@ -36,3 +36,4 @@ def find_top_N(filename, n):
 # https://www.kaggle.com/datasets/thedevastator/movie-gross-and-ratings-from-1989-to-2014
 if __name__ == "__main__":
     find_top_N("Movies_gross_rating.csv", 10)
+
